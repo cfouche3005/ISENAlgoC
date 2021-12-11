15262dba6ededa854fb05eb4cae54881fcd7abed
cfouche3005 <cfouche3005@gmail.com>
2021-12-11 13:34:09 +0100

    7

---
+++ TP3-III/exercice_1/Exo1TP3-III.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void swap(int* n1, int* n2);
+
+int main (){
+int n1 = 2;
+int n2 = 6;
+}
+
+void swap(int* n1, int* n2){
+ int temp;
+ temp = *n1;
+*n1 = *n2;
+*n2 = temp;
+}
