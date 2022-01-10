841522a1ddc3c5e7256bc7b87820557b0f6d9860
cfouche <cfouche3005@gmail.com>
2022-01-10 16:25:58 +0100

    14

--- TP6/ex2/TP6ex2.c
+++ TP6/ex2/TP6ex2.c
@@ -4,21 +4,16 @@
 
 int main (int argc, char** argv) {
     int nbmot = atoi(argv[1]);
-    char mot[nbmot][32];
-    for (int i = 0; i== nbmot; i++) {
+    char mot[10][32];
+    for (int i = 0; i<nbmot; i++) {
         printf("Enter a string :");
-        scanf("%s",&mot[i]);
+        scanf("%s",mot[i]);
 
     }
-    int motlong = 0;
-    for(int i = 0; i<nbmot; i++){
-        motlong += strlen(mot[i])+1;
-    }
-    char result[motlong];
+    printf("The sentence is :");
     for(int i = nbmot-1; i>=0; i--){
-        strcat(result, " ");
-        strcat(result, mot[i]);
+        printf(" %s",mot[i]);
     }
-    printf("The sentence is : %c",result);
+
     return 0;
 }
