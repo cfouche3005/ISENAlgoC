61beb5dd243153b7ce2a2ee5731d46370b8839bb
cfouche <cfouche3005@gmail.com>
2022-01-18 11:21:37 +0100

    16

---
+++ TP7/ex2/TP7ex22.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main (int argc, char** argv) {
+    int* pair;
+    int* impair;
+    int taillepair = 0;
+    int tailleimpair = 0;
+    int taillepairalloc = 0;
+    int tailleimpairalloc =0;
+
+
+    for (int i = 1; i < argc; ++i) {
+        if (atoi(argv[i]) % 2 == 0) {
+            taillepairalloc++;
+        }
+    }
+    for (int i = 1; i < argc; ++i) {
+        if (atoi(argv[i]) % 2 != 0) {
+            tailleimpairalloc++;
+        }
+    }
+
+    pair = malloc ((taillepairalloc)*sizeof (int) ) ;
+    impair = malloc ((tailleimpairalloc)*sizeof (int) ) ;
+
+    if (pair != NULL) {
+        for (int i = 1; i < argc; ++i) {
+            if (atoi(argv[i]) % 2 == 0) {
+                pair[taillepair] = atoi(argv[i]);
+                taillepair++;
+            }
+        }
+    }
+    if (impair != NULL) {
+        for (int i = 1; i < argc; ++i) {
+            if (atoi(argv[i]) % 2 != 0) {
+                impair[tailleimpair] = atoi(argv[i]);
+                tailleimpair++;
+            }
+        }
+    }
+
+    for(int i = 0; i< taillepair; i++){
+        printf("%d ",pair[i]);
+    }
+    printf("\n");
+    for(int i = 0; i< tailleimpair; i++){
+        printf("%d ",impair[i]);
+    }
+
+    free(pair);
+    free(impair);
+
+    return 0;
+}
